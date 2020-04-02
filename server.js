a4380fe07f1454042c90d332144c5ccb8528441a
Darren Evans <darren.evans@k4connect.com>
2020-04-01 22:54:00 -0400

    set up heroku hosting

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const path = require("path");
 const axios = require('axios');
 
 const { API_KEY } = process.env;
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/movies/:title', async(req, res) => {
   const { title } = req.params;
@@ -15,5 +16,13 @@ app.get('/movies/:title', async(req, res) => {
     res.send('error')
   }
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => `Server running on port ${port}`);
